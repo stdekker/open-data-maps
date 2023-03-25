dad1c67fb003973426a1e2b420b20cf662761685
stdekker <sdekker@sp.nl>
2023-03-26 00:21:55 +0100

    2 files modified

--- js/app.js
+++ js/app.js
@@ -1,35 +1,50 @@
-var input = document.getElementById("autocomplete-input");
-var options = document.querySelectorAll("#cities option");
+var input = document.getElementById('autocomplete-input');
+var options = document.querySelectorAll('#cities option');
 
-var map = L.map("map").setView([52.3676, 4.9041], 13);
+// Start in amsterdam
+var map = L.map('map').setView([52.3676, 4.9041], 13);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution:
+    'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+  maxZoom: 12,
+}).addTo(map);
 
-// Cache the options
-var optionsCache = Array.from(options).map(function(option) {
-  return {
-    option: option,
-    value: option.value.toLowerCase()
-  };
-});
+// Get all possible municipalities
+var cityLocations = {};
+var datalist = document.querySelector('#cities');
+
+fetch('data/cities.json')
+  .then((response) => response.json())
+  .then((data) => {
+    data.forEach((city) => {
+      cityLocations[city.city.toLowerCase()] = {
+        lat: city.lat,
+        lng: city.lng,
+      };
+    });
+  });
+
+console.log(datalist);
 
 // Attach event listener to parent element using event delegation
-var input = document.getElementById("autocomplete-input");
-input.addEventListener("input", function() {
+var input = document.getElementById('autocomplete-input');
+input.addEventListener('input', function () {
   var value = this.value.toLowerCase();
-  var options = document.querySelectorAll("#cities option");
+  var options = document.querySelectorAll('#cities option');
   for (var i = 0; i < options.length; i++) {
     var option = options[i];
     var optionValue = option.value.toLowerCase();
     if (optionValue.indexOf(value) !== -1) {
-      option.style.display = "block";
+      option.style.display = 'block';
     } else {
-      option.style.display = "none";
+      option.style.display = 'none';
     }
   }
 });
 
-input.addEventListener("change", function() {
+input.addEventListener('change', function () {
   var value = this.value.toLowerCase();
-  var options = document.querySelectorAll("#cities option");
+  var options = document.querySelectorAll('#cities option');
   for (var i = 0; i < options.length; i++) {
     var option = options[i];
     var optionValue = option.value.toLowerCase();
@@ -40,29 +55,3 @@ input.addEventListener("change", function() {
     }
   }
 });
-
-var cityLocations = {};
-
-fetch("data/cities.json")
-  .then(response => response.json())
-  .then(data => {
-    data.forEach(city => {
-      cityLocations[city.city.toLowerCase()] = {
-        lat: city.lat,
-        lng: city.lng
-      };
-    });
-  })
-  .then(() => {
-    var map = L.map("map").setView([52.3676, 4.9041], 13);
-
-    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-      maxZoom: 18,
-    }).addTo(map);
-  });
-
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-  maxZoom: 18,
-}).addTo(map);
