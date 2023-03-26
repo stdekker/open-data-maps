d2db12340d4f4dc278a7cf4823e3b106a154687c
stdekker <sdekker@sp.nl>
2023-03-26 21:49:06 +0200

    First working step with all cities

--- js/app.js
+++ js/app.js
@@ -9,49 +9,40 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 12,
 }).addTo(map);
 
-// Get all possible municipalities
-var cityLocations = {};
-var datalist = document.querySelector('#cities');
-
 fetch('data/cities.json')
   .then((response) => response.json())
   .then((data) => {
+    // Create a new object mapping city names to lat/lng values
+    const cityLocations = {};
     data.forEach((city) => {
-      cityLocations[city.city.toLowerCase()] = {
+      cityLocations[city.city] = {
         lat: city.lat,
         lng: city.lng,
       };
     });
+
+    // Use the cityLocations object here, e.g.
+    var select = document.getElementById('city');
+    for (const cityName in cityLocations) {
+      const option = document.createElement('option');
+      option.value = cityName;
+      option.text = cityName;
+      select.appendChild(option);
+    }
+    // Attach event listener to parent element using event delegation
+    var input = document.getElementById('city');
+
+    input.addEventListener('change', function () {
+      var value = this.value;
+      var options = document.querySelectorAll('#city option');
+      for (var i = 0; i < options.length; i++) {
+        var option = options[i];
+        var optionValue = option.value;
+        if (optionValue === value) {
+          var cityData = cityLocations[value];
+          map.setView([cityData.lat, cityData.lng], 13);
+          break;
+        }
+      }
+    });
   });
-
-console.log(datalist);
-
-// Attach event listener to parent element using event delegation
-var input = document.getElementById('autocomplete-input');
-input.addEventListener('input', function () {
-  var value = this.value.toLowerCase();
-  var options = document.querySelectorAll('#cities option');
-  for (var i = 0; i < options.length; i++) {
-    var option = options[i];
-    var optionValue = option.value.toLowerCase();
-    if (optionValue.indexOf(value) !== -1) {
-      option.style.display = 'block';
-    } else {
-      option.style.display = 'none';
-    }
-  }
-});
-
-input.addEventListener('change', function () {
-  var value = this.value.toLowerCase();
-  var options = document.querySelectorAll('#cities option');
-  for (var i = 0; i < options.length; i++) {
-    var option = options[i];
-    var optionValue = option.value.toLowerCase();
-    if (optionValue === value) {
-      var cityData = cityLocations[value];
-      map.setView([cityData.lat, cityData.lng], 13);
-      break;
-    }
-  }
-});
