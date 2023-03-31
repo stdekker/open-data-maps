81408c9f1c498eeccbec5690897ef2670eecd103
stdekker <sdekker@sp.nl>
2023-04-01 00:53:31 +0200

    1 file modified, 1 added

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-// Start in amsterdam
+// Start in Amsterdam
 var map = L.map('map').setView([52.3676, 4.9041], 13);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution:
@@ -6,11 +6,14 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 12,
 }).addTo(map);
 
+// Attach event listener to parent element using event delegation
+let selector = document.getElementById('city');
+let cityLocations = {};
+
 fetch('data/cities.json')
   .then((response) => response.json())
   .then((data) => {
     // Create a new object mapping city names to lat/lng values
-    const cityLocations = {};
     data.forEach((city) => {
       cityLocations[city.city] = {
         lat: city.lat,
@@ -18,28 +21,47 @@ fetch('data/cities.json')
       };
     });
 
-    // Use the cityLocations object here, e.g.
-    var select = document.getElementById('city');
-    for (const cityName in cityLocations) {
+    // Add the city Names to the select input
+    for (cityName in cityLocations) {
       const option = document.createElement('option');
       option.value = cityName;
       option.text = cityName;
-      select.appendChild(option);
+      selector.appendChild(option);
     }
-    // Attach event listener to parent element using event delegation
-    var input = document.getElementById('city');
+    selector.value = 'Amsterdam';
 
-    input.addEventListener('change', function () {
+    // Select a Municipality via the dropdown
+    selector.addEventListener('change', function () {
       var value = this.value;
       var options = document.querySelectorAll('#city option');
       for (var i = 0; i < options.length; i++) {
         var option = options[i];
-        var optionValue = option.value;
-        if (optionValue === value) {
+        if (option.value === value) {
           var cityData = cityLocations[value];
+          // Change view to Lat Long
           map.setView([cityData.lat, cityData.lng], 13);
+          // Fetch gemeentegrenzen GeoJSON-bestand
+          fetch('data/amsterdam.json')
+            .then((response) => response.json())
+            .then((data) => {
+              // Wis de bestaande gemeentegrenzen op de kaart
+              geojsonLayer.clearLayers();
+              // Voeg de nieuwe gemeentegrenzen toe aan de kaart
+              geojsonLayer.addData(data).addTo(map);
+            });
           break;
         }
       }
     });
   });
+
+var geojsonLayer = L.geoJSON(null, {
+  style: function (feature) {
+    return {
+      fillColor: 'gray',
+      fillOpacity: 0.2,
+      weight: 1,
+      color: 'black',
+    };
+  },
+});
