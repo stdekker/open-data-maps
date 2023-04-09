f12a8f3a463ff5c1bed3d47e4f3eb42ad787b867
Stijn Dekker <sdekker@sp.nl>
2023-04-09 22:22:28 +0200

    Update deploy.sh

--- .github/deploy.sh
+++ .github/deploy.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "Uploading files to $SFTP_HOST"
 
-sftp -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null -b - $SFTP_USERNAME@$SFTP_HOST <<EOF
+sftp -oStrictHostKeyChecking=no -oPort=$SFTP_PORT -oUserKnownHostsFile=/dev/null -b - $SFTP_USERNAME@$SFTP_HOST <<EOF
   cd $SFTP_PATH
   put -r -P ./*
   quit
