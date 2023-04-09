e8537adefb6d0c4efba6ad58edc0e845d7a63d14
Stijn Dekker <sdekker@sp.nl>
2023-04-09 21:56:11 +0200

    Update deploy.sh

--- .github/deploy.sh
+++ .github/deploy.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -e
 
-# Connect to SFTP server
-lftp -c "open -u $SFTP_USERNAME,$SFTP_PASSWORD $SFTP_HOST"
+echo "Uploading files to $SFTP_HOST"
 
-# Upload files
-lftp -c "mirror -R -e -x .git -x .github -x README.md -x deploy.sh $GITHUB_WORKSPACE $SFTP_PATH"
+sftp -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null -b - $SFTP_USERNAME@$SFTP_HOST <<EOF
+  cd $SFTP_PATH
+  put -r -P ./*
+  quit
+EOF
+
+echo "Deployment complete"
