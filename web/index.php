7eb3994d867dfa7e5b7e066c4b0978053aee94b3
Stijn Dekker <stijn.dekker@gmail.com>
2024-12-15 20:15:23 +0100

    Keyboard controls for sidebar

--- web/index.php
+++ web/index.php
@@ -7,28 +7,27 @@
         <title>Open Data Maps</title>
         <script src='https://api.mapbox.com/mapbox-gl-js/v2.15.0/mapbox-gl.js'></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/chroma-js/2.4.2/chroma.min.js"></script>
-
         <link href='https://api.mapbox.com/mapbox-gl-js/v2.15.0/mapbox-gl.css' rel='stylesheet' />
         <link rel="stylesheet" href="style/main.css">
     </head>
     <body>
         <div class="container">
             <div class="feature-name-box"></div>
-            <div id="map"></div>
+            <div id="map" tabindex="0" aria-label="Map"></div>
             <div class="sidebar">
                 <header><h1>Maps<sup title="Open Data">OD</sup></h1></header>
                 <div class="search-container">
-                    <input type="text" id="searchInput" placeholder="Search gemeente...">
+                    <input type="text" id="searchInput" placeholder="Search gemeente..." aria-label="Search gemeente">
                     <div id="autocompleteList" class="autocomplete-items"></div>
                     <div class="search-error">No matching municipality found</div>
                 </div>
                 <ul class="menu-items" role="tablist">
-                    <li role="tab" id="municipal-view">Gemeente</li>
-                    <li role="tab" id="national-view">Nederland</li>
+                    <li role="tab" id="municipal-view" tabindex="0" aria-selected="false">Gemeente</li>
+                    <li role="tab" id="national-view" tabindex="0" aria-selected="false">Nederland</li>
                 </ul>
                 <div class="toggle-container">
                     <label class="toggle-switch">
-                        <input type="checkbox" id="electionToggle">
+                        <input type="checkbox" id="electionToggle" tabindex="0" aria-label="Toggle Verkiezingen">
                         <span class="toggle-slider"></span>
                         <span class="toggle-label">Verkiezingen</span>
                     </label>
