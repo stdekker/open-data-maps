b4a54db0d554db2b2a235bb0e735ed11fbfc3bdd
Stijn Dekker <stijn.dekker@gmail.com>
2024-12-23 01:34:21 +0100

    Attribution typos

--- web/content/help.php
+++ web/content/help.php
@@ -8,5 +8,5 @@ header('Content-Type: text/html; charset=utf-8');
     <li>Dubbelklik op een gemeente in de landelijke weergave om in te zoomen</li>
     <li>Hover over gebieden voor meer informatie</li>
 </ul>
-<p>Open Data Maps is een project van <a href="https://www.sddkr.nl">Stijn Dekker</a>. Naar een idee van Benjamin Broersma & Bob van Vliet</p>
+<p>Open Data Maps is een project van <a href="https://www.sdkkr.nl">Stijn Dekker</a>. Naar een idee van Benjamin Broersma & <a href="https://www.bobvanvliet.nl">Bob van Vliet</a></p>
 <p>De software is open source (GPLv3) en te vinden op <a href="https://github.com/stdekker/open-data-maps">GitHub</a>. Vragen en suggesties zijn welkom!</p>
