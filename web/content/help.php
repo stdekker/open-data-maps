ae7d4c8b9408ccb68b33e883a02b234930d18837
Stijn Dekker <stijn.dekker@gmail.com>
2024-12-23 01:27:05 +0100

    Updated help and attribution

--- web/content/help.php
+++ web/content/help.php
@@ -4,9 +4,9 @@ header('Content-Type: text/html; charset=utf-8');
 <p>Welkom bij Open Data Maps. Hier zijn enkele tips om te beginnen:</p>
 <ul>
     <li>Gebruik de zoekbalk om een gemeente te vinden</li>
-    <li>Schakel tussen gemeente- en landelijke weergave met de knoppen</li>
-    <li>Activeer verkiezingsdata met de schakelaar</li>
+    <li>Via het icoon met de drie puntjes vind je de statistische instellingen.</li>
     <li>Dubbelklik op een gemeente in de landelijke weergave om in te zoomen</li>
     <li>Hover over gebieden voor meer informatie</li>
 </ul>
-<p>Voor vragen of suggesties, bezoek de <a href="https://github.com/stdekker/open-data-maps">GitHub repository</a>.</p>
+<p>Open Data Maps is een project van <a href="https://www.sddkr.nl">Stijn Dekker</a>. Naar een idee van Benjamin Broersma & Bob van Vliet</p>
+<p>De software is open source (GPLv3) en te vinden op <a href="https://github.com/stdekker/open-data-maps">GitHub</a>. Vragen en suggesties zijn welkom!</p>
