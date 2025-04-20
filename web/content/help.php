3c7197e5958fc81cfd72e24c65c313747d3431b7
Stijn Dekker <stijn.dekker@gmail.com>
2025-04-20 15:04:52 +0200

    Updated readme and help (#3)

--- web/content/help.php
+++ web/content/help.php
@@ -4,11 +4,11 @@ header('Content-Type: text/html; charset=utf-8');
 <p>Welkom bij Open Data Maps. Een korte instructie:</p>
 <ul>
     <li>Gebruik de zoekbalk om een gemeente te vinden</li>
-    <li>Via het icoon met de drie puntjes pas je de statistische instellingen van de kaart aan.</li>
+    <li>Via het icoon met de drie puntjes naast de gemeentenaam pas je de statistische instellingen van de kaart aan.</li>
     <li>Dubbelklik op een gemeente in de landelijke weergave om in te zoomen</li>
-    <li>Hover over buurten voor meer informatie.</li>
+    <li>Control + klik om een gebied op de kaart te selecteren.</li>
+    <li>Hover over gebieden voor meer informatie.</li>
 </ul>
-
 
 <p><em>Open Data Maps</em> is een project van <a href="https://www.sdkkr.nl">Stijn Dekker</a>. De Open Data is afkomstig van PDOK, CBS en de Kiesraad.</p>
 <p>De software is open source (GPLv3) en te vinden op <a href="https://github.com/stdekker/open-data-maps">GitHub</a>. Vragen en suggesties zijn welkom!</p>
